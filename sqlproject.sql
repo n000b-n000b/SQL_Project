c29b90684509494a39e80e50db00ae809842a2d8
N3XG3N <72074802+n000b-n000b@users.noreply.github.com>
2022-04-15 06:33:05 -0700

    Update sqlproject.sql

--- sqlproject.sql
+++ sqlproject.sql
@@ -16,13 +16,13 @@ Crashes_In_Last24hrs int,
 server varchar(18) 
 );
 
-insert into company_domains values (1, 'https://jwill1.test', '10.2.123.4', 'Asia');
+insert into company_domains values (1, 'https://jwill1.test', '4.4.4.4', 'Asia');
 
-insert into traffic_report values (1, date'2022-01-04', 344, 2, '10.2.123.4');
+insert into traffic_report values (1, date'2022-01-04', 344, 2, '1.2.3.4');
 
-insert into company_domains values (2, 'https://jwill2.test', '192.168.5.54', 'Europe');
+insert into company_domains values (2, 'https://jwill2.test', '2.1.5.4', 'Europe');
 
-insert into traffic_report values (2, date'2022-01-14', 587, 0, '192.168.5.54');
+insert into traffic_report values (2, date'2022-01-14', 587, 0, '9.1.5.5');
 
 select * from company_domains, traffic_report;
 
@@ -30,17 +30,17 @@ select * from traffic_report;
 
 INSERT INTO company_domains 
     WITH domain AS ( 
-        SELECT 3, 'https://jwill3.test', '10.4.56.17', 'USA'    FROM dual UNION ALL 
-        SELECT 4, 'https://jwill4.test', '164.34.45.2', 'Middle East'   FROM dual UNION ALL 
-        SELECT 5, 'https://jwill5.test', '192.168.2.5', 'Asia'  FROM dual 
+        SELECT 3, 'https://jwill3.test', '0.4.5.7', 'USA'    FROM dual UNION ALL 
+        SELECT 4, 'https://jwill4.test', '4.3.5.2', 'Middle East'   FROM dual UNION ALL 
+        SELECT 5, 'https://jwill5.test', '2.8.2.5', 'Asia'  FROM dual 
     ) 
     SELECT * FROM domain;
 
 INSERT INTO traffic_report 
     WITH report AS ( 
-        SELECT 3, date'2022-02-09', 777, 1, '10.4.56.17'    FROM dual UNION ALL 
-        SELECT 4, date'2022-02-13', 1453, 6, '164.34.45.2'  FROM dual UNION ALL 
-        SELECT 5, date'2022-02-27', 478, 14, '192.168.2.5'  FROM dual 
+        SELECT 3, date'2022-02-09', 777, 1, '1.4.6.7'    FROM dual UNION ALL 
+        SELECT 4, date'2022-02-13', 1453, 6, '4.3.4.2'  FROM dual UNION ALL 
+        SELECT 5, date'2022-02-27', 478, 14, '1.1.2.5'  FROM dual 
     ) 
     SELECT * FROM report;
 
